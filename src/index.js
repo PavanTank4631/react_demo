fd3d18118fe0fee37e2e64e95c757baaa0ac248d
pavan tank <pavantank7016@gmail.com>
2020-08-28 21:00:50 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'react-circular-progressbar/dist/styles.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 ReactDOM.render(
   <React.StrictMode>
